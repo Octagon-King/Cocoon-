42dd56a8a9b83b17a530aa0343c2874f20dfb3a0
Rahimullah Sharifi <ghamay_sharifi93@yahoo.com>
2019-01-08 17:48:51 +0000

    Added more event fragments

--- app/src/main/java/net/cocoon/cocoon/EventsFragment.java
+++ app/src/main/java/net/cocoon/cocoon/EventsFragment.java
@@ -4,15 +4,33 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class EventsFragment extends Fragment {
+    private Button button;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        View view=inflater.inflate(R.layout.fragment_events, container, false);
+        Button btnFragment=(Button) view.findViewById(R.id.buttonPhuket);
+
+        btnFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                FragmentTransaction fr=getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, new EventsFragment());
+                fr.addToBackStack(null);
+                fr.commit();
+            }
+        });
+        return view;
     }
+
+
+
 }
