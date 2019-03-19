345b5811da341559526e2cbe636525562e492c4a
Rahimullah Sharifi <ghamay_sharifi93@yahoo.com>
2019-03-19 12:23:15 +0000

    Events Added

--- app/src/main/java/net/cocoon/cocoon/EventBarcelonaFragment.java
+++ app/src/main/java/net/cocoon/cocoon/EventBarcelonaFragment.java
@@ -1,6 +1,7 @@
 package net.cocoon.cocoon;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,15 +10,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class EventBarcelonaFragment extends Fragment {
+
+    private Button tktbarcelona;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_event_barcelona,container,false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        tktbarcelona = (Button)getActivity().findViewById(R.id.tktbarcelona);
+        tktbarcelona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://www.facebook.com/events/224231028452635/"));
+                getActivity().startActivity(i);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/net/cocoon/cocoon/EventMadridFragment.java
+++ app/src/main/java/net/cocoon/cocoon/EventMadridFragment.java
@@ -1,6 +1,7 @@
 package net.cocoon.cocoon;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,14 +10,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class EventMadridFragment extends Fragment {
+
+    private Button tktmadrid;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_event_madrid,container,false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        tktmadrid = (Button)getActivity().findViewById(R.id.tktmadrid);
+        tktmadrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://www.facebook.com/events/279298926066172/"));
+                getActivity().startActivity(i);
+
+            }
+        });
+
     }
 }
