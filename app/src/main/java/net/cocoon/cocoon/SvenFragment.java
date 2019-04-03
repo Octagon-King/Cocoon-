155e33ae823e77b19ff5c66d87aacc6454639626
Rahimullah Sharifi <ghamay_sharifi93@yahoo.com>
2019-04-03 10:47:57 +0100

    Social Media links added in artist xml

--- app/src/main/java/net/cocoon/cocoon/SvenFragment.java
+++ app/src/main/java/net/cocoon/cocoon/SvenFragment.java
@@ -3,22 +3,29 @@ package net.cocoon.cocoon;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.BinderThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SvenFragment extends Fragment {
 
     private Button btnArtist;
+    private TextView TextView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sven, container, false);
+
+                return inflater.inflate(R.layout.fragment_sven, container, false);
+
+
     }
 
     @Override
